f7a691f7810260eebca96b6c1d51ad553be4a589
aomsk <plaaom39@gmail.com>
2022-02-11 02:08:55 +0700

    Delete

--- frontend/pages/_app.js
+++
@@ -1,8 +0,0 @@
-// import '../styles/globals.css'
-// import '../styles/scss/styles.scss'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
