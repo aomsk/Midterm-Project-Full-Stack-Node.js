9e84b75d845ee5276e6542409d06d5bbd3450691
aomsk <plaaom39@gmail.com>
2022-03-02 20:29:29 +0700

    upadate .next

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "587e13a0f89ae2c0"; }
+/******/ 		__webpack_require__.h = function() { return "607531581369362c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/.next/static/webpack/webpack.dfb8543b4543b14f.hot-update.js
+++ frontend/.next/static/webpack/webpack.dfb8543b4543b14f.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "0880ec013e9afc6d"; }
+/******/ 	__webpack_require__.h = function() { return "5ab773e6d825f542"; }
 /******/ }();
 /******/ 
 /******/ }
